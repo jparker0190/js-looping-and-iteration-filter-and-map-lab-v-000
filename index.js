d6b886ff34c4c6170aeac07454acfa62be8ee4a2
Joseph Parker <jparker010190@gmail.com>
2018-07-14 18:04:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ function exactMatch(driver, object){
 function exactMatchToList(driver, object){
   var t = exactMatch(driver, object)
   t.map(function(num){
+    console.log(num)
     return num.name
   })
 }
